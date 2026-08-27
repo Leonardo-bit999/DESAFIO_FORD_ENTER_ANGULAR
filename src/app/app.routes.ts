e7ec2f2b16a5124384907ba821daa0da2d7f333b
Leonardo-bit999 <leosantos.silva9925@gmail.com>
2026-08-27 00:08:37 -0300

    Primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component: Login, title:'Login - Ford'},
+    {path:'home', component: Home, title:'Home - Ford'},
+    {path:'dashboard', component: Dashboard, title:'Dashboard - Ford'},
+    {path:'', redirectTo:'/login', pathMatch:'full'}
+];
